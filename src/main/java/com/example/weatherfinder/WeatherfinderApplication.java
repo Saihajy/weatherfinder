3a719269705b876212ea5b38733a48802e9924f1
Saihaj Toor <st.812@protonmail.com>
2026-01-21 15:59:32 -0800

    init push

---
+++ src/main/java/com/example/weatherfinder/WeatherfinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weatherfinder/WeatherfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

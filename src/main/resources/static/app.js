9e5d024db585e0c63f36445af24af72318407eff
Saihaj Toor <st.812@protonmail.com>
2026-01-29 22:59:49 -0800

    finialized implementing the functionality for top10 cities

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,10 +1,18 @@
 var cities = {
     Vancouver: {lat: 49.2586, lon: -123.1207},
-    Toronto: {lat: 43.6532, lon: -79.3832}
+    Toronto: {lat: 43.6532, lon: -79.3832},
+    Montreal: {lat: 45.5031, lon: -73.5698},
+    Calgary: {lat: 51.0456, lon: -114.0575},
+    Ottawa: {lat: 45.4208, lon: -75.6901},
+    Edmonton: {lat: 53.5436, lon: -113.4912},
+    Winnipeg: {lat: 49.8955, lon: -97.1384},
+    Brampton: {lat: 43.6858, lon: -79.7599},
+    Surrey: {lat: 49.1913, lon: -122.8491},
+    Chilliwack: {lat: 49.1709, lon: -121.9525},
 }
 async function getWeather(lat, lon) {
     var url ="https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + lon + 
-    "&current=temperature_2m,relative_humidity_2m,wind_speed_10m" + "&timezone=auto";
+    "&current=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation" + "&timezone=auto";
 
     var res = await fetch(url);
     var data = await res.json();
@@ -17,7 +25,7 @@ function renderWeather(cityName, current) {
     box.innerHTML = 
     "<h2>" + cityName + "</h2>" + 
     "<p>Temp: " + current.temperature_2m + " C</p>" + "<p>Humidity: " + current.relative_humidity_2m + "%</p>" + 
-    "<p>Wind: " + current.wind_speed_10m + " km/h</p>";
+    "<p>Wind: " + current.wind_speed_10m + " km/h</p>" + "<p>Current Precipitation: " + current.precipitation + " mm</p>";
 }
 
 function attachListners() {
